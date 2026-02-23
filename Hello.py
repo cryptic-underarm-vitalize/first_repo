d5b283d966feddbb5d2f28207878ef82ce979754
Dev <shailendra10245@gmail.com>
2026-02-23 16:25:46 +0530

    h

--- Hello.py
+++
@@ -1,5 +0,0 @@
-print("Hellow 2222")
-print("Hellow 3333")
-print("Hellow 4444")
-print("ttete") 
-print("56")
--- hello.py
+++
@@ -1,9 +0,0 @@
-import time from os
-print("Hellow world")
-print("Test")
-print("ZEROOOO")
-print("KBC")
-print("CBC")
-print("KAijo")
-print("THank you")
-print("Welcome to the world of programming")
